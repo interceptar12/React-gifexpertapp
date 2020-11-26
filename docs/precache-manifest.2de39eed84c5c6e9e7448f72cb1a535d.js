f9f60a58b8ebc545536396bde326391fc7fd2a3c
interceptar12 <jaimito1189@gmail.com>
2020-11-25 18:09:26 -0600

    docs agregado

---
+++ docs/precache-manifest.2de39eed84c5c6e9e7448f72cb1a535d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081962496e6fa4a6ceb4dbec4c7e5463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d4afd3a5312347072a6",
+    "url": "/static/css/main.edf08275.chunk.css"
+  },
+  {
+    "revision": "b380063bf79d894dd30d",
+    "url": "/static/js/2.e9167b08.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.e9167b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4afd3a5312347072a6",
+    "url": "/static/js/main.d79bec99.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
